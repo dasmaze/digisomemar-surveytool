b1668b2052b130c07a7a38ff89af789a029dd76a
Mathias Lenz <lenz@htw-berlin.de>
2012-04-23 20:46:29 +0300

    Moved the question template to its own partial and did some layout stuff.

--- apps/frontend/modules/survey/templates/showSuccess.php
+++ apps/frontend/modules/survey/templates/showSuccess.php
@@ -1,3 +1,5 @@
-<div class="alert alert-error"><?php echo $sf_user->getFlash('error') ?></div>
+<?php if($sf_user->hasFlash('error')): ?>
+    <div class="alert alert-error"><?php echo $sf_user->getFlash('error') ?></div>
+<?php endif ?>
 <h2><?php echo $surveyTitle ?></h2>
 <p><?php echo $surveyDesc ?></p>
