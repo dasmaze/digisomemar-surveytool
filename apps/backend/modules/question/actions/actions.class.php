dbcb43bdfeb21630277f08b9f895f650206fefc9
Markus Konrad <post@mkonrad.net>
2012-04-23 15:45:51 +0300

    creating new questions to surveys works

--- apps/backend/modules/question/actions/actions.class.php
+++ apps/backend/modules/question/actions/actions.class.php
@@ -32,6 +32,54 @@ class questionActions extends autoQuestionActions
         parent::executeIndex($request);  
     } 
     
+  protected function processForm(sfWebRequest $request, sfForm $form)
+  {
+      $data = $request->getParameter($form->getName());
+      $data['survey_id'] = $this->getUser()->getAttribute('currentSurveyId');
+      
+      $form->bind($data, $request->getFiles($form->getName()));
+       
+    if ($data['survey_id'] && $form->isValid())
+    {
+      $notice = $form->getObject()->isNew() ? 'The item was created successfully.' : 'The item was updated successfully.';
+
+      try {
+        $question = $form->save();
+      } catch (Doctrine_Validator_Exception $e) {
+
+        $errorStack = $form->getObject()->getErrorStack();
+
+        $message = get_class($form->getObject()) . ' has ' . count($errorStack) . " field" . (count($errorStack) > 1 ?  's' : null) . " with validation errors: ";
+        foreach ($errorStack as $field => $errors) {
+            $message .= "$field (" . implode(", ", $errors) . "), ";
+        }
+        $message = trim($message, ', ');
+
+        $this->getUser()->setFlash('error', $message);
+        return sfView::SUCCESS;
+      }
+
+      $this->dispatcher->notify(new sfEvent($this, 'admin.save_object', array('object' => $question)));
+
+      if ($request->hasParameter('_save_and_add'))
+      {
+        $this->getUser()->setFlash('notice', $notice.' You can add another one below.');
+
+        $this->redirect('@question_new');
+      }
+      else
+      {
+        $this->getUser()->setFlash('notice', $notice);
+
+        $this->redirect(array('sf_route' => 'question_edit', 'sf_subject' => $question));
+      }
+    }
+    else
+    {
+      $this->getUser()->setFlash('error', 'The item has not been saved due to some errors.', false);
+    }
+  }
+    
   protected function getPager()
   {
     $pager = $this->configuration->getPager('Question');
@@ -43,4 +91,6 @@ class questionActions extends autoQuestionActions
 
     return $pager;
   }
+  
+  
 }
