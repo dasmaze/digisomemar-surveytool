adf03caf26b4295edcd9e6fa1695cc21cf556a72
Philipp <philipp@philipp-laptop.(none)>
2012-04-24 14:52:11 +0200

    class for submit form

--- apps/frontend/modules/survey/templates/_questionPartial.php
+++ apps/frontend/modules/survey/templates/_questionPartial.php
@@ -3,7 +3,10 @@
 	    <h3><?php echo $question['text'];  ?></h3>
 	    <form>
 	    <?php foreach($question['answers'] as $answer) { ?>
-	        <input type="<?php echo $question['type'] ?>" name="<?php echo 'answer' . $question['id'] ?>" value="<?php echo $answer['id'] ?>">
+	        <input type="<?php echo $question['type'] ?>" 
+	        name="<?php echo 'answer' . $question['id'] ?>" 
+	        value="<?php echo $answer['id'] ?>"
+	        class="answerForm">
 	            <?php echo $answer['text'] ?>
 	        </input>
 	    <?php } ?>
