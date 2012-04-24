dcfcc8ae877e3a4b78aac85f7da532ac833a252e
Markus Konrad <post@mkonrad.net>
2012-04-24 14:54:51 +0300

    made Question model timestampable / schema changed

--- lib/filter/doctrine/base/BaseQuestionFormFilter.class.php
+++ lib/filter/doctrine/base/BaseQuestionFormFilter.class.php
@@ -17,6 +17,8 @@ abstract class BaseQuestionFormFilter extends BaseFormFilterDoctrine
       'discription' => new sfWidgetFormFilterInput(),
       'multichoice' => new sfWidgetFormChoice(array('choices' => array('' => 'yes or no', 1 => 'yes', 0 => 'no'))),
       'survey_id'   => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Survey'), 'add_empty' => true)),
+      'created_at'  => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+      'updated_at'  => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
     ));
 
     $this->setValidators(array(
@@ -24,6 +26,8 @@ abstract class BaseQuestionFormFilter extends BaseFormFilterDoctrine
       'discription' => new sfValidatorPass(array('required' => false)),
       'multichoice' => new sfValidatorChoice(array('required' => false, 'choices' => array('', 1, 0))),
       'survey_id'   => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('Survey'), 'column' => 'id')),
+      'created_at'  => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+      'updated_at'  => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
     ));
 
     $this->widgetSchema->setNameFormat('question_filters[%s]');
@@ -48,6 +52,8 @@ abstract class BaseQuestionFormFilter extends BaseFormFilterDoctrine
       'discription' => 'Text',
       'multichoice' => 'Boolean',
       'survey_id'   => 'ForeignKey',
+      'created_at'  => 'Date',
+      'updated_at'  => 'Date',
     );
   }
 }
