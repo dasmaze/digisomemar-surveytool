44b3a5cb8cc534ab1b54531b49f1e34a32d4c4d6
Philipp <philipp@philipp-laptop.(none)>
2012-04-24 14:55:13 +0200

    class to form

--- apps/frontend/modules/survey/templates/_questionPartial.php
+++ apps/frontend/modules/survey/templates/_questionPartial.php
@@ -1,12 +1,11 @@
 <?php foreach($questionArray as $question) { ?>
 	<div>
 	    <h3><?php echo $question['text'];  ?></h3>
-	    <form>
+	    <form class="answerForm">
 	    <?php foreach($question['answers'] as $answer) { ?>
 	        <input type="<?php echo $question['type'] ?>" 
 	        name="<?php echo 'answer' . $question['id'] ?>" 
-	        value="<?php echo $answer['id'] ?>"
-	        class="answerForm">
+	        value="<?php echo $answer['id'] ?>">
 	            <?php echo $answer['text'] ?>
 	        </input>
 	    <?php } ?>
