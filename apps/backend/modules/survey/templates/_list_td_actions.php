f1345b639240cb17ecbb51a00bc283d403a0096a
Markus Konrad <post@mkonrad.net>
2012-04-24 16:00:08 +0300

    added routing for results

--- apps/backend/modules/survey/templates/_list_td_actions.php
+++ apps/backend/modules/survey/templates/_list_td_actions.php
@@ -2,6 +2,6 @@
   <ul class="sf_admin_td_actions">
     <?php echo $helper->linkToEdit($survey, array(  'params' =>   array(  ),  'class_suffix' => 'edit',  'label' => 'Edit',)) ?>
     <?php echo $helper->linkToDelete($survey, array(  'params' =>   array(  ),  'confirm' => 'Are you sure?',  'class_suffix' => 'delete',  'label' => 'Delete',)) ?>
-    <?php echo link_to("Show results", "survey/results?id=") ?>
+    <?php echo link_to("Show results", "results/" . $survey->getId()) ?>
   </ul>
 </td>
